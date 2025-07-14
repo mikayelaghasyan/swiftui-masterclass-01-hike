6b73f6154d2d9b95b7a96a5b8ad823fb84c65799
Mikayel Aghasyan <mikayel.aghasyan@gmail.com>
2025-07-14 15:33:50 +0400

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Mikayel Aghasyan on 14.07.25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
